b73bbfc709507a8b6077411875f6edd965d82866
firjanlukynhas <firjanlukynhas@gmail.com>
2025-05-22 13:30:44 -0300

    Add files via upload

---
+++ in.js
@@ -0,0 +1,21 @@
+//Manual para iniciar projeto Node.js com Express
+//1. Abra o CMD e navegue até Desktop
+//cd desktop
+//2. Crie uma pasta que ficará seu projeto
+//mkdir nome_da_pasta
+//3. Entre na pasta criado anteriormente
+//cd nome_da_pasta
+//4. Inicie projeto Node
+//npm init -y
+//5. Instale o Express
+//npm install express
+//6. Instale o nodemon globalmente e depois dentro do seu projeto
+//npm install -g nodemon
+//npm install nodemon
+//7. Abra o projeto no VS Code
+//code .
+//8. Crie um arquivo index.js na raiz do projeto
+//9. Inicie um servidor express dentro do arquivo index.js
+//10.Execute o servidor pelo Prompt de Comando
+//npx nodemon index.js
+//11.Teste a rota http://localhost:3000 no Postman
